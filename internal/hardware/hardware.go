be1b1df6878745d4e50ab95f6680eb37e73c4803
Sugoto Basu <60142374+Sugoto@users.noreply.github.com>
2024-07-10 15:30:59 +0530

    Update hardware.go

--- internal/hardware/hardware.go
+++ internal/hardware/hardware.go
@@ -14,6 +14,7 @@ import (
 const megabyteDiv uint64 = 1024 * 1024
 const gigabyteDiv uint64 = megabyteDiv * 1024
 
+// Get system info
 func GetSystemSection() (string, error) {
 	runTimeOS := runtime.GOOS
 
